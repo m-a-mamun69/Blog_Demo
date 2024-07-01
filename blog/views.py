0e1073e30ffb332f1a326baf1ef1e8ef7418404c
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-01 12:06:01 +0600

    Createing Models And Adding Two Files also necessary Stuff

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html", {"name":"Mamun"})
+    return render(request, "index.html")
+
+
