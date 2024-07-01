1dee7ea9e1f76212027f00032f0da55fb32baa7b
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-01 17:21:01 +0600

    Getting Post On the Page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts":posts})
 
 
