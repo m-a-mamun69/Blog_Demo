7b705b6c5df4ca7d102ec1509c79f3c2e2594f34
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-02 13:04:34 +0600

    Adding Two Files And Unsolved Error 'Getting Data'.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('post/<str:pk>/', views.getPost, name="post_detail"),
+    path('post/<str:pk>/delete/', views.deletePost, name="post_delete"),
+    path('new_post/', views.createPost, name="create_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
@@ -7,4 +7,28 @@ def index(request):
     posts = Post.objects.all()
     return render(request, "index.html", {"posts":posts})
 
+def getPost(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, "post_detail.html", {"post":post})
+
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+    post.delete()
+    return redirect('home')
+
+
+def createPost(request):
+    context = {}
+    if request.method == "POST":
+        try:
+            Post.objects.create(
+                title = request.POST.get("title"),
+                body = request.POST.get("description"),
+                autor = request.user
+                )
+            return redirect("home")
+        except:
+            context["message"] = "*Invalid details"
+
+    return render(request, "new_post.html", context)
 
