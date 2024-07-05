c7dac2439f62e6dac419e6f4df3d9a0d3b34b16c
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-05 17:06:16 +0600

    add Another Model 'Comment' and Comment Functionality

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.comment
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
